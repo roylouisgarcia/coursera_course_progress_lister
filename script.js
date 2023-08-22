3af01646938dc907611fc2590d410bf853f17447
Roy Louis Garcia <roygarcia201@gmail.com>
2023-08-22 06:22:00 -0700

    hid the list of courses

---
+++ script.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+	console.log("ready");
+$(body).css('background-color', 'red');
+// $(".ibm_cybersecurity_analyst_professional_certificate_button").click(function(){
+// $(".ibm_cybersecurity_analyst_professional_certificate").show("fast", function(){});
+// console.log("clicked")
+// })
+});
+
+$( window ).on( "load", function() {
+   console.log("window ready");
+});
