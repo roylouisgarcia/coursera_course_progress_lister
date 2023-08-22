24e0385b33ae0372901f42b51539963d824e1ecd
Roy Louis Garcia <roygarcia201@gmail.com>
2023-08-22 13:00:26 -0700

    success in implementing pure toggle switch to show and hide courses

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
-  $("#ibm_cybersecurity_analyst_professional_certificate_button").click(function () {
-    $("#ibm_cybersecurity_analyst_professional_certificate_list").show(
-      "slow",
-      function () {}
-    );
-    $("#ibm_cybersecurity_analyst_professional_certificate_list").removeClass(
-      "courses_list_display_none"
-    );
-    $("#ibm_cybersecurity_analyst_professional_certificate_list").addClass(
-      "courses_list_display_flex"
-    );
-    $("#ibm_cybersecurity_analyst_professional_certificate_list").css(
-      "display",
-      "flex"
-    );
-    document.getElementById("ibm_cybersecurity_analyst_professional_certificate_button").innerHTML = "Hide Courses";
-  });
+  let toggle_elements = document.querySelectorAll(".toggle_courses");
+
+  for (toggle_element of toggle_elements) {
+    toggle_element.addEventListener("click", function () {
+      if (!this.dataset.clicked) {
+        this.setAttribute("data-clicked", "true");
+        this.style.backgroundColor = "#fff";
+        this.style.color = "#000";
+        this.innerHTML = "Hide Courses";
+        this.parentElement.nextElementSibling.classList.remove(
+          "courses_list_display_none"
+        );
+      } else {
+        this.removeAttribute("data-clicked");
+        this.removeAttribute("style");
+        this.parentElement.nextElementSibling.classList.add(
+          "courses_list_display_none"
+        );
+        this.innerHTML = "Show Courses";
+      }
+    });
+  }
 });
